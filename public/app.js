e7637170ae0d60f2d32fc209291a2447213ce095
Deren Cinar <108646259+DerenCinar@users.noreply.github.com>
2025-09-07 15:38:00 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -29,8 +29,8 @@ const app = Vue.createApp({
                 {displayName: 'D2 - Besser Schreiben', name:'french5', book: 'D'},
                 {displayName: 'D2 - Besser Schreiben PT4', name:'french_pt_4', book: 'D'},
                 {displayName: 'A2 - 2/6/25', name:'A1-SPA', book: 'D'},
-                {displayName: 'FRA - Activities', name:'active', book: 'D'}
-                {displayName: 'FRA 8 - Test 1', name:'french8test1', book: 'D'}
+                {displayName: 'FRA - Activities', name:'active', book: 'D'},
+                {displayName: 'FRA 8 - Test 1', name:'french8test1', book: 'D'},
 
                 
             ],
